4fc406a8d07fa9859b49e39cc64d29d62df836a3
heiman <castroheiman26@gmail.com>
2023-11-12 18:40:51 -0500

    aqui llegue hasta la parte de link y cambiar una clase de forma dinamica

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] }); // tipo de letra y idioma
+
+export const lusitana = Lusitana({  // tipo de fuente tipo de grosor de la letra y idioma
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
